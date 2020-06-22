7b0b7266b396011fc75d983c54e359ddd4f647d4
Issa Kalonji <ikalonji@c5r1s6.wethinkcode.co.za>
2020-06-22 14:46:41 +0200

    ex00 done

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -6,35 +6,22 @@
 /*   By: ikalonji <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/18 10:03:17 by ikalonji          #+#    #+#             */
-/*   Updated: 2020/06/19 09:13:43 by ikalonji         ###   ########.fr       */
+/*   Updated: 2020/06/22 14:34:25 by ikalonji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_print_alphabet(void);
-
-void ft_putchar(char c){
-
-	write(1, &c, 1);
-	
-}
-
-int main(void){
-
-	ft_print_alphabet();
-return 0;
-
-}
-
-
-void ft_print_alphabet(void){
+void ft_print_alphabet(void)
+{
 	
 	char letter;
    	letter = 'a';
 
-	while(letter <= 'z'){
+	while(letter <= 'z')
+	{
 		ft_putchar(letter);
 	  	letter++;
 	}				
 }
+
--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -6,20 +6,23 @@
 /*   By: ikalonji <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 10:53:01 by ikalonji          #+#    #+#             */
-/*   Updated: 2020/06/19 11:12:28 by ikalonji         ###   ########.fr       */
+/*   Updated: 2020/06/19 15:24:23 by ikalonji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
 void ft_is_negative(int n){
-	if(n>0){
-		write(1,"P", sizeof(char));
-	}else;
-	write(1, "N", sizeof(char));
+	
+	if(n>=0){
+		write(1,"P\n", 2);
+		if(n<0){
+			write(1, "N\n", 2);
+			}
+}
 }
 
 int main(){
-	ft_is_negative(-4);
+	ft_is_negative(5);
 
 		return 0;
 }
