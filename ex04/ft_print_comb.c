0368b2a0997e5eeb4ecf9216cb1bcda75fc7e44f
Issa Kalonji <ikalonji@c5r1s6.wethinkcode.co.za>
2020-06-22 14:48:50 +0200

    ex04 done

--- ex04/ft_print_comb.c
+++ ex04/ft_print_comb.c
@@ -6,16 +6,36 @@
 /*   By: ikalonji <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 11:26:13 by ikalonji          #+#    #+#             */
-/*   Updated: 2020/06/19 15:51:16 by ikalonji         ###   ########.fr       */
+/*   Updated: 2020/06/22 14:38:55 by ikalonji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char c){
-	write(1, &c, sizeof &c);
+void ft_helper(int a, int b, int c)
+{
+	if (a == 10) 
+	return;
+    else if (b == 10) ft_helper(a + 1, a + 2, a + 3);
+    else if (c == 10) ft_helper(a, b + 1, b + 2);
+    else
+{
+    ft_putchar(',');
+    ft_putchar(' ');
+    ft_putchar("0123456789"[a]);
+    ft_putchar("0123456789"[b]);
+    ft_putchar("0123456789"[c]);
+    ft_helper(a, b, c + 1);
+}
+}
+
+void ft_print_comb(void)
+{
+    ft_putchar('0');
+    ft_putchar('1');
+    ft_putchar('2');
+    ft_helper(0, 1, 3);
+  	ft_putchar('\n');
 }
 
 
-
-	
