e6e6a3ddeaaf141b436d7bc8e2926f6c09ef1059
Issa Kalonji <ikalonji@c5r1s6.wethinkcode.co.za>
2020-06-22 14:47:51 +0200

    ex02 done

--- ex02/ft_print_numbers.c
+++ ex02/ft_print_numbers.c
@@ -6,27 +6,20 @@
 /*   By: ikalonji <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 10:07:54 by ikalonji          #+#    #+#             */
-/*   Updated: 2020/06/19 10:48:14 by ikalonji         ###   ########.fr       */
+/*   Updated: 2020/06/22 14:35:47 by ikalonji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char c){
-	write(1, &c, sizeof c);
-}
-
-void ft_print_numbers(void){
-	int num=060;
-		while(num<=071){
+void ft_print_numbers(void)
+{
+	int num;
+	num	=060;
+		while(num<=071)
+		{
 			ft_putchar(num);
 			num++;
 		}
 }
 
-int main(){
-
-	ft_print_numbers();
-
-	return 0;
-}
