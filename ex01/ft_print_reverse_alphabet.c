a82f5c7922b6055e4f046c9b9dd5f994f34ed303
Issa Kalonji <ikalonji@c5r1s6.wethinkcode.co.za>
2020-06-22 14:47:22 +0200

    ex01 done

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -6,27 +6,20 @@
 /*   By: ikalonji <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 09:25:16 by ikalonji          #+#    #+#             */
-/*   Updated: 2020/06/19 10:05:21 by ikalonji         ###   ########.fr       */
+/*   Updated: 2020/06/22 14:33:34 by ikalonji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-void ft_putchar(char c){
-	write(1, &c, 1);
-}
-
-void ft_print_reverse_alphabet(void){
+void ft_print_reverse_alphabet(void)
+{
     char letter;
 	letter ='z';
-	while(letter>='a'){
+	while(letter>='a')
+	{
 		ft_putchar(letter);
 		letter--;
 	}
 }
 
-int main(){
-	ft_print_reverse_alphabet();
 
-	return 0;
-}
